8ee81a0774ce86cc997a473642c372bcb4fcd6a8
anisha-shaik976 <anishashaik007@gmail.com>
2026-06-23 23:27:55 +0530

    Task 4 - Advanced Features and Testing

--- app/src/main/java/com/example/studenttaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/studenttaskmanager/MainActivity.kt
@@ -30,7 +30,18 @@ fun LoginScreen() {
 
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var students by remember { mutableStateOf(listOf<User>()) }
+
+    var students by remember {
+        mutableStateOf(listOf<User>())
+    }
+
+    var isLoading by remember {
+        mutableStateOf(false)
+    }
+
+    var errorMessage by remember {
+        mutableStateOf("")
+    }
 
     Column(
         modifier = Modifier
@@ -67,28 +78,64 @@ fun LoginScreen() {
 
         Button(
             onClick = {
-                RetrofitClient.apiService.getUsers()
-                    .enqueue(object : Callback<List<User>> {
 
-                        override fun onResponse(
-                            call: Call<List<User>>,
-                            response: Response<List<User>>
-                        ) {
-                            students = response.body() ?: emptyList()
-                        }
+                if (username == "admin" && password == "1234") {
 
-                        override fun onFailure(
-                            call: Call<List<User>>,
-                            t: Throwable
-                        ) {
-                        }
-                    })
+                    isLoading = true
+                    errorMessage = ""
+
+                    RetrofitClient.apiService.getUsers()
+                        .enqueue(object : Callback<List<User>> {
+
+                            override fun onResponse(
+                                call: Call<List<User>>,
+                                response: Response<List<User>>
+                            ) {
+
+                                isLoading = false
+
+                                if (response.isSuccessful) {
+                                    students = response.body() ?: emptyList()
+                                } else {
+                                    errorMessage = "Failed to load data"
+                                }
+                            }
+
+                            override fun onFailure(
+                                call: Call<List<User>>,
+                                t: Throwable
+                            ) {
+
+                                isLoading = false
+                                errorMessage = "Failed to load data"
+                            }
+                        })
+
+                } else {
+
+                    errorMessage = "Invalid Username or Password"
+                    students = emptyList()
+                }
             }
         ) {
             Text("Load Students")
         }
 
         Spacer(modifier = Modifier.height(20.dp))
+
+        if (isLoading) {
+            CircularProgressIndicator()
+        }
+
+        if (errorMessage.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = errorMessage
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         students.forEach {
             Text(text = it.name)
